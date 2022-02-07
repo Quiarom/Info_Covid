b7605022485005ac6228bfa133390763e9967cce
Quiarom <userquiarom@gmail.com>
2022-02-07 19:12:31 -0400

    Add and Integrate React Router to the Project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <h1>Hecho con ❤</h1>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+       <div>
+           <h1>Hola Mundo</h1>
+       </div>
+    ) 
+}
+
+export default Home
